af8dca8de4f72a2611bb039f3198dca567152818
Atlas <atlas@atlas-systems.uk>
2026-06-17 13:51:25 +0100

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,11 @@
-import { cloudflareTest } from "@cloudflare/vitest-pool-workers";
-import { defineConfig } from "vitest/config";
+import { defineWorkersProject } from "@cloudflare/vitest-pool-workers/config";
 
-export default defineConfig({
-  plugins: [
-    cloudflareTest({
-      wrangler: { configPath: "./wrangler.toml" },
-    }),
-  ],
-});
+export default defineWorkersProject(() => ({
+  test: {
+    poolOptions: {
+      workers: {
+        wrangler: { configPath: "./wrangler.toml" },
+      },
+    },
+  },
+}));
